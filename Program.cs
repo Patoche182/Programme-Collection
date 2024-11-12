4c8d0d41f9fce8379e482e61714bf51ae08c1ab8
Patrice Hanne Expleo <patrice.hanne@expleogroup.com>
2024-11-12 18:49:15 +0100

    Programme Collections - Tableaux Init

--- Program.cs
+++ Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+
+        static void Tableaux()
+        {
+            //int a = 5;
+            //a = 10;
+
+            int[] t = new int[3];
+            t[0] = 2;
+            t[1] = 4;
+            t[2] = 1;
+            t[2]++;
+
+            //Console.WriteLine(t[2]);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(t[i]);
+
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tableaux !");
+            Tableaux();
         }
     }
 }
