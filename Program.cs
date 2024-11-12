439a8c52bb581eb3d510789209c20dd3e9911b0b
Patrice Hanne Expleo <patrice.hanne@expleogroup.com>
2024-11-12 18:38:30 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programme_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
