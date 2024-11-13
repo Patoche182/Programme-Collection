5b08a302f6f86a3bd720a87960c5f66dab37dc09
Patrice Hanne Expleo <patrice.hanne@expleogroup.com>
2024-11-13 10:21:23 +0100

    Tableaux OK

--- Program.cs
+++ Program.cs
@@ -20,23 +20,29 @@
             //int a = 5;
             //a = 10;
 
-            // Tableaux => Array
-            int[] t = new int[6];
-            t[0] = 2;
-            t[1] = 4;
-            t[2] = 1;
-            t[3] = 5;
-            t[4] = 5;
-            t[5] = 3;
+            // Tableaux => Array -> Taille FIXE !!!!
+            //int[] t = new int[6];
+            //t[0] = 2;
+            //t[1] = 4;
+            //t[2] = 1;
+            //t[3] = 5;
+            //t[4] = 5;
+            //t[5] = 3;
+
+            //int[] t = { 2, 4, 1, 5, 5, 3 }; // Notation equivalente
+
+            //t[0] = 10;
 
             //Console.WriteLine(t[2]);
+            string[] t = { "Martin", "Emilie", "Paul" };
+            Console.WriteLine(t[0][0]); // Affiche le 1er élément et le 1er charcarter de cet élément -> M
 
             for (int i = 0; i < t.Length; i++)
             {
                 Console.WriteLine(t[i]);
 
             }
-            Console.WriteLine("Somme : " + SommeTableau(t));
+            //Console.WriteLine("Somme : " + SommeTableau(t));
 
         }
         static void Main(string[] args)
